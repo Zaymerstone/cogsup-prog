5345dbc1bdd53d605add80f454df774fff765d2c
Egor Nesterov <egor.nesterov2002@gmail.com>
2025-10-01 17:59:15 +0200

    Added class work week 4

---
+++ Week-4/Exercises/drawing_functions.py
@@ -0,0 +1,54 @@
+from expyriment import design, control, stimuli
+import random
+
+def load(stims):
+    for stim in stims:
+        stim.preload()
+
+def timed_draw(stims):
+    t0 = exp.clock.time
+    for stim in stims:
+        stim.present(clear=False)
+    exp.screen.update()
+    t1 = exp.clock.time
+    return t1 - t0
+    # return the time it took to draw
+
+def present_for(stims, t=1000):
+    for stim in stims:
+        stim.present(clear=False)
+    exp.screen.update()
+    exp.clock.wait(t)
+    exp.screen.clear()
+    exp.screen.update()
+
+
+""" Test functions """
+exp = design.Experiment()
+
+control.set_develop_mode()
+control.initialize(exp)
+
+fixation = stimuli.FixCross()
+load([fixation])
+
+n = 20
+positions = [(random.randint(-300, 300), random.randint(-300, 300)) for _ in range(n)]
+squares = [stimuli.Rectangle(size=(50, 50), position = pos) for pos in positions]
+load(squares)
+
+durations = []
+
+t0 = exp.clock.time
+for square in squares:
+    if not square.is_preloaded:
+        print("Preloading function not implemneted correctly.")
+    stims = [fixation, square] 
+    present_for(stims, 500)
+    t1 = exp.clock.time
+    durations.append(t1-t0)
+    t0 = t1
+
+print(durations)
+
+control.end()
